38f108a11dc4897fa0c77f41cb0c64fb169f077c
Kush Patel <kdpatel@unc.edu>
2022-01-21 21:53:36 -0500

    "Progress on Exercise 1"

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,15 +3,14 @@
 __author__ = "730477807"
 
 five_character_word: str = input("Enter a 5-character word: ")
+#if len(five_character_word) != 5:
+#    print("Error: Word must contain 5 characters")
+#    exit()
 
-if len(five_character_word) != 5:
-    print("Error: Word must contain 5 characters")
-    exit()
-else:
-    single_character: str = input("Enter a single character: ")
-    if len(single_character) != 1:
-        print("Error: Character must be a single character.")
-        exit()
+single_character: str = input("Enter a single character: ")
+#if len(single_character) != 1:
+#    print("Error: Character must be a single character.")
+#    exit()
 
 matching_characters: int = 0
 
